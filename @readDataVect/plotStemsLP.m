dbdea758d61e07f17aa5bee7d5d7dfe14cba414a
Dmytro S Lituiev <d.lituiev@gmail.com>
2014-11-05 15:15:21 +0100

    a minor clean-up

--- @readDataVect/plotStemsLP.m
+++ @readDataVect/plotStemsLP.m
@@ -1,40 +1,41 @@
+function plotStemsLP(obj, varargin)
 
-        function plotStemsLP(obj, varargin)
-            if nargin>2 && isscalar(varargin{1}) && isnumeric(varargin{1})
-                chr0 = varargin{1};
-                fieldPlotFun = @(x)obj.plotOneChromosome(chr0, x{:});
-                args = varargin(2:end);
-            else
-                chr0 = (1:obj.chrNumber)';
-                fieldPlotFun = @(x)obj.plotChromosomes(x{:});
-                args = varargin;
-            end
-            markerSz = 4;
-            
-            %             plotChromosomes(obj, 'xPosteriorNorm', ...
-            %                 'plotfun', @(x,y)stem(x,y, 'MarkerSize', markerSz, 'MarkerEdgeColor', 'r', 'linewidth', 0.8),... % 'MarkerEdgeColor', 'r',
-            %                 'exp10',false, 'yscale', 'lin', 'figure', 'new', varargin{:});
-            %
-            %             set(obj.prevLine(:, end), 'BaseValue',( obj.prevYLims(1)-2) );
-            
-            fieldPlotFun({'xPselNorm', ...
-                'plotfun', @(x,y)stem(x,y, 'MarkerSize', markerSz-1, 'MarkerEdgeColor', 'g', 'linewidth', 0.8),... % 'MarkerEdgeColor', 'r',
-                'exp10',false, 'yscale', 'lin', 'yThr', 0, 'figure', 'new', args{:}});
-            
-            set(obj.prevLine(chr0, end), 'BaseValue',( obj.prevYLims(1)-2) );
-            
-            hits = (obj.xPosteriorNorm > obj.xPselNorm );
-            colors = bsxfun(@times, [0 .8 0], hits) + bsxfun(@times, [.2 1 .2], 0.5 * ~hits) ;
-            
-            fieldPlotFun({ 'xPselNorm', ...
-                'plotfun', @(x,y,z)scatter(x,y, markerSz, colors(z)),... % 'MarkerEdgeColor', 'r',
-                'exp10',false, 'yscale', 'lin',  args{:}});
-            
-            colors = bsxfun(@times, [.8 0 0], hits) ;% + bsxfun(@times, [1 0.2 0.2], 0.2 * ~hits) ;
-            fieldPlotFun({'xPosteriorNorm', ...
-                'plotfun', @(x,y,z)scatter(x,y, markerSz, colors(z)),... % 'MarkerEdgeColor', 'r',
-                'exp10',false, 'yscale', 'lin',  args{:}});
-            
-            
-            set(obj.prevAxes(chr0, end), 'TickDir', 'out')
-        end
+if nargin>2 && isscalar(varargin{1}) && isnumeric(varargin{1})
+    chr0 = varargin{1};
+    fieldPlotFun = @(x)obj.plotOneChromosome(chr0, x{:});
+    args = varargin(2:end);
+else
+    chr0 = (1:obj.chrNumber)';
+    fieldPlotFun = @(x)obj.plotChromosomes(x{:});
+    args = varargin;
+end
+    
+markerSz = 4;
+
+%             plotChromosomes(obj, 'xPosteriorNorm', ...
+%                 'plotfun', @(x,y)stem(x,y, 'MarkerSize', markerSz, 'MarkerEdgeColor', 'r', 'linewidth', 0.8),... % 'MarkerEdgeColor', 'r',
+%                 'exp10',false, 'yscale', 'lin', 'figure', 'new', varargin{:});
+%
+%             set(obj.prevLine(:, end), 'BaseValue',( obj.prevYLims(1)-2) );
+
+fieldPlotFun({'xPselNorm', ...
+    'plotfun', @(x,y)stem(x,y, 'MarkerSize', markerSz-1, 'MarkerEdgeColor', 'g', 'linewidth', 0.8),... % 'MarkerEdgeColor', 'r',
+    'exp10',false, 'yscale', 'lin', 'yThr', 0, 'figure', 'new', args{:}});
+
+set(obj.prevLine(chr0, end), 'BaseValue',( obj.prevYLims(1)-2) );
+
+hits = (obj.xPosteriorNorm > obj.xPselNorm );
+colors = bsxfun(@times, [0 .8 0], hits) + bsxfun(@times, [.2 1 .2], 0.5 * ~hits) ;
+
+fieldPlotFun({ 'xPselNorm', ...
+    'plotfun', @(x,y,z)scatter(x,y, markerSz, colors(z)),... % 'MarkerEdgeColor', 'r',
+    'exp10',false, 'yscale', 'lin',  args{:}});
+
+colors = bsxfun(@times, [.8 0 0], hits) ;% + bsxfun(@times, [1 0.2 0.2], 0.2 * ~hits) ;
+fieldPlotFun({'xPosteriorNorm', ...
+    'plotfun', @(x,y,z)scatter(x,y, markerSz, colors(z)),... % 'MarkerEdgeColor', 'r',
+    'exp10',false, 'yscale', 'lin',  args{:}});
+
+
+set(obj.prevAxes(chr0, end), 'TickDir', 'out')
+end
--- scr_obj_vect.m
+++ scr_obj_vect.m
@@ -2,8 +2,9 @@ close all; clear all; clc;
 dbclear if warning
 tic
 
-% DATA_PATH = '/media/Processing/seq/data';
-DATA_PATH = './raw_data';
+GRAPH_EXPORT_TYPE = 'pdf'; % 'eps'
+DATA_PATH = '/media/Processing/seq/data';
+% DATA_PATH = './raw_data';
 USERFNCT_PATH = './dependencies';
 addpath(genpath(USERFNCT_PATH));
 savepath;
@@ -18,14 +19,10 @@ addpath('./emission');
 %= + number of individuals in the mapping population (N)
 dataID = 'HL7/p889_20110125_HL7_Paired-rmdup-clipOverlap-q20-ems-annotation'; x0 = 5672441; chr0 = 1; N = 50;
 
-linkageLoosening = 1;
-% AR.Alpha = 1./(0:0.01:1);
+linkageLoosening = 1; % 1./(0:0.01:1);
 
 %%
 disp(['=======  Processing data from the run ''', dataID, ''' ======='])
-
-%= and genetic distance in cM between them)
-
 
 
 %= construct the path to the (primary experimental) data file
@@ -54,18 +51,15 @@ AR.filterFields('q', @(x)(x>7)); % mutant reads
 AR.calcDxMin;
 f = AR.visualizeStat;
 fig(f, 'width', 24)
-exportfig(gcf, fullfile('figures', dataID,'qualityCtrl'), 'format','eps', 'color', 'rgb')
+exportfig(gcf, fullfile('figures', dataID,'qualityCtrl'), 'format',GRAPH_EXPORT_TYPE, 'color', 'rgb')
 
 % visualizeAnnotationStat(annotation)
 
-
 AR.filterFields('r', @(x)(x<quantile(AR.r, 0.98))); % mutant reads
 AR.filterFields('q', @(x)(x>7)); % mutant reads
-
-% AR = AR.filterFields('f', @(x)(x<1)); % SNP ratio
 AR.filterFields('f', @(x)(x<.8)); % SNP ratio
 
-% AR = AR.filterFields('dx', @(x)(x>120)); % SNP ratio
+% AR = AR.filterFields('dx', @(x)(x>120)); % SNP spacing
 mixtObj = AR.unmix('plot');
 f = plotReadSpacingPDF(AR, mixtObj, 'all');
 clear mixtObj
@@ -74,19 +68,14 @@ AR.filterDx();
 AR.plotChromosomes('dx', 'yscale', 'log', 'figure', 'new', 'plotfun', @(x,y)plot(x,y, 'rx'));
 AR.plotChromosomes('dxFiltered', 'yscale', 'log', 'figure', 'old', 'plotfun', @(x,y)plot(x,y, 'm-'));
 
-% AR = AR.filterFields('dxFiltered', @(x)(x>70491)); % SNP ratio
-%% SNP ratio
-
+%% plot SNP ratio
 AR.clearPlots;
 
-KERNEL = 0;
+KERNEL = 0; % 81;
 AR.plotSnpRatio(KERNEL)
 fig(gcf, 'width', 24)
 
-% KERNEL = 81;
-% AR.plotSnpRatio(KERNEL)
-% fig(gcf, 'width', 24)
-exportfig(gcf, fullfile('figures', dataID, sprintf('SNP_Ratio_k%u', KERNEL)), 'format','eps', 'color', 'rgb')
+exportfig(gcf, fullfile('figures', dataID, sprintf('SNP_Ratio_k%u', KERNEL)), 'format',GRAPH_EXPORT_TYPE, 'color', 'rgb')
 
 %% load the linkage map
 mapPath = './reference/ChrMap.mat';
@@ -109,22 +98,16 @@ AR.pop = N;
 theta = 1e-2;
 lambda1 = 1;
 
-% AR.includeRnaPresence()
-
 AR.emissionHandle = @(q, r, study)emissionMixBetaBinomial(q, r, AR.pop, theta, lambda1);
 % AR.emissionHandle = @(q, r, study)emissionMixBetaBinomial(q, r, AR.pop.N, theta, gi1(1:AR.Mtot));
-
-%  AR.emissionHandle = @(q, r, study)emissionBetaBinomial(q, r, study, theta);
+% AR.emissionHandle = @(q, r, study)emissionBetaBinomial(q, r, study, theta);
 % AR.emissionHandle = @(qq, rr, ff)emissionk0(qq, rr, AR.pop);
 
-% AR.Alpha = 1./(0:0.01:1);
 AR.Alpha =  linkageLoosening;
 
 AR.plotChromosomes('contrib', 'yscale', 'lin', 'norm', true, 'figure', 'new', 'plotfun', @(x,y)plot(x,y, 'r^', 'markersize', 6, 'linewidth', 2));
 
 AR.runHMM();
-
-% AR.xPsel = calcMarginal(AR.xkPflat(:,floor(end/2):end),2);
 
 AR.normalizeChromosomes;
 
@@ -134,26 +117,25 @@ AR.plotChromosomes('xPstat', 'yscale', 'lin', 'norm', true);
 
 z = AR.calcMleZ('plot');
 fig(gcf, 'width', 24)
-exportfig(gcf, fullfile('figures', dataID, 'zMLE'), 'format','eps', 'color', 'rgb')
+exportfig(gcf, fullfile('figures', dataID, 'zMLE'), 'format',GRAPH_EXPORT_TYPE, 'color', 'rgb')
 
 if exist('chr0', 'var')
     AR.plotChromosome2D(chr0);
 end
 
-% AR.calcLogOdds;
 AR.plotChromosomes('xLogOdds', 'yscale', 'lin', 'figure', 'new', 'yThr', 0);
 
 fig(gcf, 'width', 24)
-exportfig(gcf, fullfile('figures', dataID,'LogLiOdds'), 'format','eps', 'color', 'rgb')
-%%
+exportfig(gcf, fullfile('figures', dataID,'LogLiOdds'), 'format', GRAPH_EXPORT_TYPE, 'color', 'rgb')
+%% plot likelihood and posterior
 AR.plotStemsLP( 'ylim', [-20,0])
 if exist('chr0', 'var')
     ind = (AR.x==x0 & AR.chromosome == chr0);    
     AR.plotChromosomes('xPselNorm', 'yscale', 'lin', 'figure', 'old', 'plotfun',...
-        @(x,y)plot(x,y, 'r^', 'markersize', 6, 'linewidth', 2), 'select', ind, 'ylim', [-20,0]);
+        @(x,y)plot(x,y, 'r^', 'markersize', 6, 'linewidth', 2), 'select', ind);
 end
 fig(gcf, 'width', 24)
-exportfig(gcf, fullfile('figures', dataID, 'LH-Posterior'), 'format','eps', 'color', 'rgb')
+exportfig(gcf, fullfile('figures', dataID, 'LH-Posterior'), 'format',GRAPH_EXPORT_TYPE, 'color', 'rgb')
 
 %%
 AR.plotChromosomes('xPselNorm', 'yscale', 'lin', 'figure', 'new', 'yThr', 0);
@@ -165,7 +147,7 @@ AR.plotChromosomes('xPosteriorNorm', 'yscale',  'lin','figure', 'old', 'yThr', 0
 numberOfHits = 2e6;
 [~,dataName,ext] = fileparts(dataID);
 AR.printTopHits(fullfile('figures', dataID, [dataName,ext, '-out.csv']), numberOfHits, 'xLogOdds', 'cutoffValue', -Inf)
-%%
+%% plot misc stuff for the selected chromosome: crowdedness
 if exist('chr0', 'var')
     figure('name', 'dx and membership')
     scatter(AR.x(AR.chromosome == chr0)*1e-6, AR.dx(AR.chromosome == chr0), 2.5+2.5*AR.contrib(AR.chromosome == chr0), AR.contrib(AR.chromosome == chr0), 'o')
@@ -174,13 +156,3 @@ if exist('chr0', 'var')
     figure('name', 'membership')
     plot(AR.x(AR.chromosome == chr0)*1e-6, AR.contrib(AR.chromosome == chr0), 'bx-')
 end
-
-%%
-
-% AR.plotStems('xPosteriorNorm', 'figure', 'new');
-% AR.plotStems('xPselNorm', 'yThr', 0);
-
-%
-% E = AR.E;
-% contrib = AR.contrib;
-% save('emission_obj', 'E', 'contrib')
